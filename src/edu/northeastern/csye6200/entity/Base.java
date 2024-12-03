a2847464d27b3a83d9c3fa5a3c05093b23c9d3ed
AoAO <64264590+Leo-AO-99@users.noreply.github.com>
2024-12-03 14:18:53 -0500

    UML (#8)

--- src/edu/northeastern/csye6200/entity/Base.java
+++ src/edu/northeastern/csye6200/entity/Base.java
@@ -2,4 +2,12 @@ package edu.northeastern.csye6200.entity;
 
 abstract class Base {
     protected int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
